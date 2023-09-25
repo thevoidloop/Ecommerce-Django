b2379b66d00bd5d5e553892e3ad393cc6012746b
Daniel Marroquin <marroquin_daniel@outlook.com>
2023-09-25 13:07:13 -0600

    Configuracion de path de archivos staticos

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -127,3 +127,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     'greatkart/static',
 ]
+
+
+# media files configurations
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
