fbcc7d851bd14ec4ac4b71b095c48446551a3a84
Alejandro Morales <acaalm3@miumg.com.gt>
2023-10-10 02:09:54 -0600

    :memo: Primera parte del paginado, para definir la cantidad de productos por pagina. :memo: - Video 89

--- store/views.py
+++ store/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
 from carts.models import CartItem
+
 from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 
@@ -17,10 +19,13 @@ def store(request, category_slug=None):
     
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
         
     }
