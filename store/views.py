399b091d78ad0acac0de9d70765ee17d226af7f8
Alejandro Morales <acaalm3@miumg.com.gt>
2023-10-10 19:56:23 -0600

    :mag: Se agrego la función de búsqueda :mag: - Video 92

--- store/views.py
+++ store/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
 from carts.models import CartItem
+from django.db.models import Q
 
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -51,3 +53,15 @@ def product_details(request, category_slug, product_slug):
     }
 
     return render(request, 'store/product_details.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products' : products,
+        'product_count': product_count,
+    }
+    return render (request, 'store/store.html' , context)
