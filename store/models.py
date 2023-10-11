9efcf2745d3331279af6f332deebd34cbeeef1ca
Alejandro Morales <acaalm3@miumg.com.gt>
2023-10-11 01:38:57 -0600

    :pencil2:  Se configuraron los  modelos de variaciones, color y size. :pencil2:: - Video 96

--- store/models.py
+++ store/models.py
@@ -22,6 +22,13 @@ class Product(models.Model):
         def __str__(self):
                 return self.product_name
 
+class VariationManager(models.Manager):
+        def colors(self):
+                return super(VariationManager, self).filter(variation_category='color', is_active=True)
+        
+        def sizes(self):
+                return super(VariationManager, self).filter(variation_category='size', is_active=True)
+
 variation_category_choice = (
         ('color', 'color'),
         ('size', 'size'),
@@ -35,5 +42,7 @@ class Variation(models.Model):
         is_active          = models.BooleanField(default=True)
         created_date       = models.DateTimeField(auto_now=True)
         
+        objects = VariationManager()
+
         def __unicod__(self):
                 return self.product
