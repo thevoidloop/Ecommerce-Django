c920859734dc22d9c1d287c4c32b19c9c0af5382
Daniel Marroquin <marroquin_daniel@outlook.com>
2023-10-18 09:43:45 -0600

    🚧 Se cambio la vista al panel de ordenes en el dashboar, tambien se agrego los detalles de los productos solicitados

--- orders/views.py
+++ orders/views.py
@@ -29,6 +29,31 @@ def payments(request):
     order.is_ordered = True
     order.save()
 
+
+    #Move the cart items to Order Product table
+
+    cart_items = CartItem.objects.filter(user=request.user)
+
+    for item in cart_items:
+        orderproduct = OrderProduct()
+        orderproduct.order_id = order.id
+        orderproduct.payment = payment
+        orderproduct.user_id = request.user.id
+        orderproduct.product_id = item.product_id
+        orderproduct.quantity = item.quantity
+        orderproduct.product_price = item.product.price
+        orderproduct.ordered = True
+        orderproduct.save()
+
+
+    #Reduce the quantity of sold producto
+
+    #Clear cart
+
+    #Send order recieved email to customer
+
+    #Send order number and transaction id back to sendData method via JsonResponse
+
     return render(request, 'orders/payments.html')
 
 
