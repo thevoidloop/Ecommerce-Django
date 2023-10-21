881d37fa87cf48a3aa67e59905692b750fa9693e
Daniel Marroquin <marroquin_daniel@outlook.com>
2023-10-20 18:56:23 -0600

    ✨ Se muestra las reviews de los usuarios

--- store/views.py
+++ store/views.py
@@ -9,6 +9,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpResponse
 from .forms import ReviewForm
 from django.contrib import messages
+from orders.models import OrderProduct
 
 # Create your views here.
 
@@ -48,11 +49,27 @@ def product_details(request, category_slug, product_slug):
         
     except Exception as e:
         raise e
-    
+
+    try:
+        orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+    except OrderProduct.DoesNotExist:
+        orderproduct = None
+
+    # Get the reviews
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
+
+    # Get the product gallery
+    # product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
     context = {
         'single_product': single_product,
         'in_cart'       : in_cart,
+        'orderproduct': orderproduct,
+        'reviews': reviews,
+        # 'product_gallery': product_gallery,
+
     }
+
 
     return render(request, 'store/product_details.html', context)
 
