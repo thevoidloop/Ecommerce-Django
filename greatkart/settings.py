4bc9584c445c9294b67a2552accdab1f1edcee8e
Alejandro Morales <acaalm3@miumg.com.gt>
2023-10-23 04:24:42 -0600

    :alien:Agregue host permitida, almacene información secreta e implemente.:alien::-Video 169

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [django-greatkart-env.eba-4ex2hxpp.us-west-2.elasticbeanstalk.com]
 
 
 # Application definition
