52d0f6bc59e712e12dae491c95999f60aba466f2
Alejandro Morales <acaalm3@miumg.com.gt>
2023-10-23 03:00:21 -0600

    :alien:Implementación - Actualización de paquetes.:alien::-Video 162 al 166

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -165,7 +165,7 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 
-
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 #EMAIL_HOST = config('EMAIL_HOST')
 #EMAIL_PORT = config('EMAIL_PORT', cast=int)
